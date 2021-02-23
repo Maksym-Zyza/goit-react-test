4b85dc121b49b1d2d14ebdb8af96cc139d131708
Maksym Zyza <mmziza88@gmail.com>
2021-02-23 21:01:10 +0200

    Зібрано проект та встановлені пакети

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+
+      <h1> Hello React!</h1>      
+      {/* <UserList user={user} /> */}
+
+
+    </div>
+  );
+};
+
+export default App;
